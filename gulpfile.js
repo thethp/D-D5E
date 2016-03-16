8bba9f32fa03ba4d5e105b14119331580f1617ad
Todd Page <todd.page@digintent.com>
2016-03-16 16:06:14 -0500

    browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,9 @@ var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 var babel = require('babelify');
 
+//browsersync things
+var browserSync = require('browser-sync').create();
+
 //css-y things
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
@@ -24,29 +27,38 @@ var mochaPhantomjs = require('gulp-mocha-phantomjs');
 var paths = {
 	scripts_app: './app/**/*.js',
 	scripts_test: './test/**/*.js',
-	styles: '.app/sass/*.scss'
+	styles: './app/sass/**/*.scss'
 };
  
+//get sassy
 gulp.task('sass', function () {
-	gulp.src(paths.styles)
+	gulp.src('./app/sass/core.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer())
 		.pipe(gulp.dest('./public/styles/'));
 });
 
+//browsersync
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: "./"
+    });
+});
+
 //browserify tasks
 gulp.task('browserify-app', ['lint-app'], function() {
-  return gulp.src('app/index.js')
+  return gulp.src('./app/scripts/index.js')
     .pipe(browserify({
       insertGlobals: true
     }))
     .pipe(rename('build.js'))
     .pipe(gulp.dest('build'))
-    .pipe(gulp.dest('public/scripts'));
+    .pipe(gulp.dest('public/scripts'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('browserify-test', ['lint-app'], function() {
-  return gulp.src('test/index.js')
+gulp.task('browserify-test', ['lint-test'], function() {
+  return gulp.src('./test/index.js')
     .pipe(browserify({
       insertGlobals: true
     }))
@@ -71,18 +83,15 @@ gulp.task('lint-test', function() {
 
 //default gulpy things
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts_app, ['browserify-app', 'test']);
+  gulp.watch([paths.scripts_app, paths.styles], ['build']);
   gulp.watch(paths.scripts_test, ['test']);
 });
 
-gulp.task('default', function() {
-	gulp.run('sass');
-	gulp.run('lint');
-	gulp.watch(paths.styles, ['sass']);
-	gulp.watch(paths.scripts, ['lint']);
-}); 
+gulp.task('build', ['browser-sync', 'sass', 'browserify-app']);
 
 gulp.task('test', ['lint-test', 'browserify-test'], function() {
   return gulp.src('test/index.html')
     .pipe(mochaPhantomjs());
 });
+
+gulp.task('default', ['test', 'build', 'watch']);
