e2c07d9fa411436d1b1220594890919447e57428
Todd Page <todd.page@digintent.com>
2016-04-14 11:10:22 -0500

    add rest of functions

--- app/scripts/actions/CharacterActions.js
+++ app/scripts/actions/CharacterActions.js
@@ -15,7 +15,64 @@ var CharacterActions = {
       actionType: AppConstants.UPDATE_CHAR_RACE,
       data: race
     });
+  },
+
+  updateCharLevel: function(level){
+    AppDispatcher.handleAction({
+      actionType: AppConstants.UPDATE_CHAR_LEVEL,
+      data: level
+    });
+  },
+
+  updateBaseHP: function(hp){
+    AppDispatcher.handleAction({
+      actionType: AppConstants.UPDATE_BASE_HP,
+      data: hp
+    });
+  },
+
+  updateBaseStrength: function(strength){
+    AppDispatcher.handleAction({
+      actionType: AppConstants.UPDATE_BASE_STRENGTH,
+      data: strength
+    });
+  },
+
+  updateBaseDexterity: function(dexterity){
+    AppDispatcher.handleAction({
+      actionType: AppConstants.UPDATE_DEXTERITY,
+      data: dexterity
+    });
+  },
+
+  updateBaseConstitution: function(constitution){
+    AppDispatcher.handleAction({
+      actionType: AppConstants.UPDATE_CONSTITUTION,
+      data: constitution
+    });
+  },
+
+  updateBaseIntelligence: function(intelligence){
+    AppDispatcher.handleAction({
+      actionType: AppConstants.UPDATE_INTELLIGENCE,
+      data: intelligence
+    });
+  },
+
+  updateBaseWisdom: function(wisdom){
+    AppDispatcher.handleAction({
+      actionType: AppConstants.UPDATE_WISDOM,
+      data: wisdom
+    });
+  },
+
+  updateBaseCharisma: function(charisma){
+    AppDispatcher.handleAction({
+      actionType: AppConstants.UPDATE_CHARISMA,
+      data: charisma
+    });
   }
+
 }
 
 export default CharacterActions;
